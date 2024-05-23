fcbe6ea0ffb217942528b1fceacae450522cbce8
charly <wastinglove1@outlook.com>
2024-05-23 14:09:11 -0300

    se agrego milisegundos en generar codigo

--- src/controllers/generate.codigo.controller.ts
+++ src/controllers/generate.codigo.controller.ts
@@ -3,7 +3,8 @@ export function generateCode() {
     const horas = fechaActual.getHours()
     const minutos = fechaActual.getMinutes()
     const segundos = fechaActual.getSeconds()
+    const mili = fechaActual.getMilliseconds()
 
-    const fecha = String(`${horas.toString(16)}${minutos.toString(16)}${segundos.toString(16)}`)
+    const fecha = String(`${horas.toString(16)}${minutos.toString(16)}${segundos.toString(16)}${ mili.toString(16)}`)
     return fecha
 }
