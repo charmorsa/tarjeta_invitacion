264e87b81bbef7ccf4496d3645c1a7ef0904c7d7
charly <wastinglove1@outlook.com>
2024-08-26 08:22:43 -0300

    version nueva 2

--- src/controllers/generate.codigo.controller.ts
+++ src/controllers/generate.codigo.controller.ts
@@ -1,26 +1,10 @@
 export function generateCode() {
-    const fechaActual = new Date()
-    const dia = fechaActual.getDay()
+    const fechaActual = new Date();
     const horas = fechaActual.getHours()
     const minutos = fechaActual.getMinutes()
     const segundos = fechaActual.getSeconds()
     const mili = fechaActual.getMilliseconds()
 
-    const fecha = Number(`${dia}${horas}${minutos}${segundos}${mili}`)
-    const code = convertirANumeroAlfanumerico(fecha)
-    return code
-}
-
-
-function convertirANumeroAlfanumerico(valor: number): string {
-    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    const base = caracteres.length;
-    let resultado = '';
-    
-    do {
-        resultado = caracteres.charAt(valor % base) + resultado;
-        valor = Math.floor(valor / base);
-    } while (valor > 0);
-    
-    return resultado;
+    const fecha = String(`${horas.toString(16)}${minutos.toString(16)}${segundos.toString(16)}${ mili.toString(16)}`)
+    return fecha
 }
