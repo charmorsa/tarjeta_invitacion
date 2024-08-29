0a82b066e2c31f4440794912a4ab9ea3d0014f8e
charly <wastinglove1@outlook.com>
2024-08-29 14:54:17 -0300

    version 1.0.8

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -7,32 +7,20 @@ export const messGuests =async (req:Request, res:Response) => {
         const code = req.body.codigo
         const datos = await inv.findOne({codigo:code})
         if(!datos) return respJson(res,400,false,{msg:'No se encontro invitado'})
-        const mess = `🎉 ¡Hola ${datos.nombre}! 🎉
-
-        ¡Nos alegra mucho anunciarte que nos vamos a casar! 💍✨
-        
-        Queremos compartir este momento especial con vos, por eso te invitamos a nuestra boda.
-        
-        🗓 Fecha: 27/12
-        🕒 Hora: 19:30hs
-        📍 Lugar de la Ceremonia: Quinta La Juliana
-        
-        
-        Para tu comodidad, por favor utiliza el Código de Invitación para confirmar tu asistencia : ${datos.codigo}
-        
-        Asistencia:
-        Confirma tu asistencia antes del 01/11 Entrando a https://nuestra-boda-natuycharly.web.app/
-        
-        Por cualquier duda nos pueden mandar un mensaje
-        
-        Natu: 376-4119665
-        Charly: 376-4814854.
-        
-        
-        Estamos emocionados de celebrar este día tan especial con vos. ¡Esperamos verte allí!
-        
-        Con cariño,
-        Nathalie Bucher y Carlos Herrera`
+        const mess = `🎉 ¡Hola ${datos.nombre}! 🎉<br/><br/>
+        ¡Nos alegra mucho anunciarte que nos vamos a casar! 💍✨<br/><br/>
+        Queremos compartir este momento especial con vos, por eso te invitamos a nuestra boda.<br/><br/>
+        🗓 Fecha: 27/12<br/>
+        🕒 Hora: 19:30hs<br/>
+        📍 Lugar de la Ceremonia: Quinta La Juliana<br/><br/>
+        Para tu comodidad, por favor utiliza el Código de Invitación para confirmar tu asistencia : ${datos.codigo}<br/><br/>
+        Confirma tu asistencia antes del 01/11 Entrando en el siguiente link: https://nuestra-boda-natuycharly.web.app/<br/><br/>
+        Por cualquier duda nos pueden mandar un mensaje<br/><br/>
+        Natu: 376-4119665<br/>
+        Charly: 376-4814854.<br/><br/>
+        Estamos emocionados de celebrar este día tan especial con vos. ¡Esperamos verte allí!<br/><br/>
+        Con cariño,<br/>
+        Natu y Charly`
         return respJson(res,200,true,{msg:mess})
     } catch (error) {
         console.log(error)
